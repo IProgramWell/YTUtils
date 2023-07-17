fa499123b3c25fbe8ba4ea08266ec3aa168e3b58
IProgramWell <couldntgetemail@mail.com>
2023-07-17 23:22:29 +0300

    So I did NOT fix that vulnerability.

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,4 +1,4 @@
-import { PageModule, ModuleUtils } from "userscriptbase/modules";
+import { PageModule, /* ModuleUtils */ } from "userscriptbase/modules";
 
 // import * as playlistStats from "./playlistStats";
 import * as noShorts from "./noShorts";
@@ -15,7 +15,7 @@ export default function createModuleList(options: Pick<PageModule, "utils">): Pa
 				onDocumentStart: noShorts.noShortsOnURLChange,
 			},
 			methods: { "yt-navigate-start": noShorts.noShortsOnNavigate, },
-			shouldBeActive: ModuleUtils.activateForRegex(/^\/shorts\/.+$/i),
+			shouldBeActive: /^\/shorts\/.+$/i,
 			utils: options.utils,
 			moduleName: "No Shorts Redirector",
 		}),
@@ -40,20 +40,19 @@ export default function createModuleList(options: Pick<PageModule, "utils">): Pa
 			},
 			shouldBeActive: noPlaylist.shouldBeActiveFor,
 			utils: options.utils,
-			moduleName: "No Playlist Controls"
+			moduleName: "No Playlist Controls",
 		}),
 		new PageModule({
 			eventHandlers: {
 				onModuleStart: searchByTitle.addSearchBtn,
 				onModuleStop: searchByTitle.removeSearchBtn,
 			},
-			shouldBeActive: ModuleUtils.activateForRegex(/^\/watch\/?$/),
+			shouldBeActive: /^\/watch\/?$/,
 			moduleName: "Search by title",
 			utils: options.utils,
 		}),
 		new PageModule({
 			eventHandlers: { init: noAds.addListeners, },
-			shouldBeActive: ModuleUtils.activateForRegex(/^\/(results|watch)?\/?$/),
 			moduleName: "No (more) banner ads",
 			utils: options.utils,
 		}),
